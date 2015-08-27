58919d2084c0865e9aeff3bee284bc6055ff2c92
Andrew Bredow <bredow@gmail.com>
2015-08-26 22:24:09 -0400

    Bump Erlang, Elixir versions

--- bootstrap/environments/elixirs.sh
+++ bootstrap/environments/elixirs.sh
@@ -3,7 +3,7 @@
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source ${SCRIPT_PATH%/*}/include.sh
 
-read -p "Install Erlang 17.4?" -n 1 -r
+read -p "Install Erlang 18.0?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   e_rocket "Installing/updating kerl"
@@ -12,24 +12,24 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   sudo wget https://raw.githubusercontent.com/spawngrid/kerl/master/kerl -N -P /opt/bin
   sudo chmod a+x /opt/bin/kerl
 
-  e_rocket "Installing Erlang 17.4"
-  KERL_CONFIGURE_OPTIONS="--enable-darwin-64bit --with-ssl=/usr/local/opt/openssl" kerl build 17.4 17.4
+  e_rocket "Installing Erlang 18.0"
+  KERL_CONFIGURE_OPTIONS="--enable-darwin-64bit --with-ssl=/usr/local/opt/openssl" kerl build 18.0 18.0
 
   mkdir -p /Users/$USER/.kerl/installs
-  kerl install 17.4 /Users/$USER/.kerl/installs/17.4
+  kerl install 18.0 /Users/$USER/.kerl/installs/18.0
 fi
 
 echo
 
-read -p "Install Elixir 1.0.2?" -n 1 -r
+read -p "Install Elixir 1.0.5?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   e_rocket "Installing Elixir"
   HOME=/Users/$USER
   curl -sSL https://raw.githubusercontent.com/taylor/kiex/master/install | bash -s
-  . /Users/$USER/.kerl/installs/17.4/activate
+  . /Users/$USER/.kerl/installs/18.0/activate
   source /Users/$USER/.kiex/scripts/kiex
-  kiex install 1.0.2
-  kiex default 1.0.2
+  kiex install 1.0.5
+  kiex default 1.0.5
 fi
 
